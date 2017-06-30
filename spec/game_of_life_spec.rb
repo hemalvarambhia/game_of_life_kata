c693602a65ae4729abe3ef5c374d0b303b56955b
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-06-30 16:27:49 +0100

    Changed the predicates to closely reflect the rules of the game.

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,12 +1,12 @@
 describe 'Game of Life' do
   def next_generation(world)
     neighbours = [ world[0][1], world[1][0], world[1][1] ]
-    if neighbours.count { |cell| cell == '.' } >= 2
+    if neighbours.count { |cell| cell == '*' } < 2
       world[0][0] = '.'
     end
 
     neighbours = [ world[0][0], world[1][0], world[1][1] ]
-    if neighbours.count { |cell| cell == '.' } >= 2
+    if neighbours.count { |cell| cell == '*' } < 2
       world[0][1] = '.'
     end
     
