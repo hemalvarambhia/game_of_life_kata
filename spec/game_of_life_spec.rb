d5c83788b3a7518b4649688d0f803efdbcccc991
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-06-30 16:00:08 +0100

    A live cell with 3 dead neighbours dies in the next generation; refactored to remove the divergent change smell.

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,4 +1,13 @@
 describe 'Game of Life' do
+  def next_generation(world)
+    neighbours = [ world[0][1], world[1][0], world[1][1] ]
+    if neighbours.count { |cell| cell == '.' } == 3
+      world[0][0] = '.'
+    end
+      
+    world
+  end
+
   describe 'A dead cell with 3 dead neighbouring cells' do
     let(:world) do
       [
@@ -7,10 +16,6 @@ describe 'Game of Life' do
       ]
     end
 
-    def next_generation(world)
-      world
-    end
-    
     it 'remains dead in the next generation' do
       cell_in_next_gen = next_generation(world)[0][0]
       
@@ -23,6 +28,19 @@ describe 'Game of Life' do
         [ next_world[0][1], next_world[1][0], next_world[1][1] ]
       
       expect(cells_in_next_gen).to all eq '.'
+    end
+  end
+
+  describe 'A live cell with 3 dead neighbouring cells' do
+    it 'dies in the next generation' do
+      world = [
+        '*.',
+        '..'
+      ]
+      
+      cell_in_next_gen = next_generation(world)[0][0]
+      
+      expect(cell_in_next_gen).to eq '.'
     end
   end
 end
