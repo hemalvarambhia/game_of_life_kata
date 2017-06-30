101b283416f28da4766cd6311c82b06a3c75a196
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-06-30 16:04:20 +0100

    Added a missing spec- the dead neighbouring cells remain so in the next generation; refactored the duplicate set up to a helper method and improved the name of a variable.

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -24,23 +24,32 @@ describe 'Game of Life' do
 
     it 'has 3 dead neighbours in the next generation' do
       next_world = next_generation(world)
-      cells_in_next_gen =
+
+      next_gen_neighbours =
         [ next_world[0][1], next_world[1][0], next_world[1][1] ]
-      
-      expect(cells_in_next_gen).to all eq '.'
+      expect(next_gen_neighbours).to all eq '.'
     end
   end
 
   describe 'A live cell with 3 dead neighbouring cells' do
-    it 'dies in the next generation' do
-      world = [
+    let(:world) do
+      [
         '*.',
         '..'
       ]
-      
+    end
+    it 'dies in the next generation' do
       cell_in_next_gen = next_generation(world)[0][0]
       
       expect(cell_in_next_gen).to eq '.'
+    end
+
+    it 'has 3 dead neighbours in the next generation' do
+      next_world = next_generation(world)
+
+      next_gen_neighbours =
+        [ next_world[0][1], next_world[1][0], next_world[1][1] ]
+      expect(next_gen_neighbours).to all eq '.'
     end
   end
 end
