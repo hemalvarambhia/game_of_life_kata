e2fa7e93c83cea74dca057491295740ab307f413
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-06-30 16:17:10 +0100

    When a live cell has 1 live neighbour and 2 dead ones, it dies in the next generation.

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,7 +1,7 @@
 describe 'Game of Life' do
   def next_generation(world)
     neighbours = [ world[0][1], world[1][0], world[1][1] ]
-    if neighbours.count { |cell| cell == '.' } == 3
+    if neighbours.count { |cell| cell == '.' } >= 2
       world[0][0] = '.'
     end
       
@@ -51,5 +51,23 @@ describe 'Game of Life' do
         [ next_world[0][1], next_world[1][0], next_world[1][1] ]
       expect(next_gen_neighbours).to all eq '.'
     end
+  end
+
+  describe 'A live cell with 1 live neighbour and 2 dead ones' do
+    it 'dies in the next generation' do
+      world = [
+        '**',
+        '..'
+      ]
+      cell_in_next_gen = next_generation(world)[0][0]
+      
+      expect(cell_in_next_gen).to eq '.'
+    end
+
+    describe 'The neighbouring live cell' do
+      it 'also dies in the next generation'
+    end
+
+    it 'continues to have 2 neighbouring dead cells'
   end
 end
