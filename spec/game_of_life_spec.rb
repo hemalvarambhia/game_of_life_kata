6a644bac3dd81f5fdf5929c695e30002a6c9d4eb
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-06-30 15:21:06 +0100

    All neighbouring dead cells remain dead in the next generation; refactored to remove set up duplication and divergent change smell.

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,15 +1,28 @@
 describe 'Game of Life' do
   describe 'A dead cell with 3 dead neighbouring cells' do
+    let(:world) do
+      [
+        '..',
+        '..'
+      ]
+    end
+
+    def next_generation(world)
+      world
+    end
+    
     it 'remains dead in the next generation' do
-      world =
-        [
-          '..',
-          '..'
-        ]
-      
-      cell_in_next_gen = world[0][0]
+      cell_in_next_gen = next_generation(world)[0][0]
       
       expect(cell_in_next_gen).to eq '.'
+    end
+
+    it 'has 3 dead neighbours in the next generation' do
+      next_world = next_generation(world)
+      cells_in_next_gen =
+        [ next_world[0][1], next_world[1][0], next_world[1][1] ]
+      
+      expect(cells_in_next_gen).to all eq '.'
     end
   end
 end
