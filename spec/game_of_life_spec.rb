030affd5b4caf6755480676ed9cab7266ed2bc81
Hemal N Varambhia <h.n.varambhia@gmail.com>
2017-07-01 15:34:27 +0100

    Refactor - extracted an intent-revealing method.

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,16 +1,20 @@
 describe 'Game of Life' do
   def next_generation(world)
     neighbours = [ world[0][1], world[1][0], world[1][1] ]
-    if neighbours.count { |cell| cell == '*' } < 2
+    if neighbours.count { |cell| alive?(cell) } < 2
       world[0][0] = '.'
     end
 
     neighbours = [ world[0][0], world[1][0], world[1][1] ]
-    if neighbours.count { |cell| cell == '*' } < 2
+    if neighbours.count { |cell| alive?(cell) } < 2
       world[0][1] = '.'
     end
     
     world
+  end
+
+  def alive?(cell)
+    cell == '*'
   end
 
   describe 'A dead cell with 3 dead neighbouring cells' do
